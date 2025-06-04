b5b48a8e87b229acd8dee3ce86e8fceb19b155e3
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-06-04 21:47:16 +0000

    constants_updated.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan Tian
+# Created on: June 03, 2025
+# This program includes constants.
+
+# This pybadge is 160 X 128 pixels
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
+
+# new pallet for red filled text
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+                b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
